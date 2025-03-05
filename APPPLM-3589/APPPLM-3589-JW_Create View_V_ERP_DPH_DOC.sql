f5b3c6a0887ffec96dea2e0670790552955a2327
Jesco.Wurm <jesco.wurm@marel.com>
2025-03-05 16:30:08 +0100

    [main] Reorganization

--- APPPLM-3589/APPPLM-3589-JW_Create View_V_ERP_DPH_DOC.sql
+++ APPPLM-3589/APPPLM-3589-JW_Create View_V_ERP_DPH_DOC.sql
@@ -1,12 +1,10 @@
 /**
-Initial version of the view V_PLM_DPH_DOC.
-Written by Jesco Wurm (ICP) on 04-03-2025
+Initial version of the view V_ERP_DPH_DOC.
+Written by Jesco Wurm (ICP) on 05-03-2025
 for APPPLM-3589 - As a PLM Administrator, I want to adapt the ECO Documents queue to show proper OriginalFileName for interfacing with DPH.
 */
-CREATE OR REPLACE FORCE EDITIONABLE VIEW "V_PLM_DPH_DOC"
-("ECO_NUMBER", "ECO_DOC_INTGR_JOB_STATUS", "ECO_DOC_INTGR_JOB_ERR_MESS", 
-   "DOC_CID", "DOCUMENT_ID", "DOC_VERSION", "PMT_STBN_ENG", "FREE_NAME", "DOC_TYPE", "DOC_SUBTYPE", 
-   "CAX_TYPE", "FILE_FORMAT", "FILE_CID", "EDB_ID", "ORG_FILE_NAME")
+CREATE OR REPLACE FORCE EDITIONABLE VIEW "V_ERP_DPH_DOC"
+("DOC_CID", "DOC_TYPE", "DOC_SUBTYPE", "CAX_TYPE", "SHEET_NO", "FILE_FORMAT", "FILE_CID", "FILE_POS_NO", "ORG_FILE_NAME", "FILE_TYPE")
 DEFAULT COLLATION "USING_NLS_COMP" 
 AS
 WITH ORG_FILE_NAME AS (
@@ -70,23 +68,17 @@ WITH ORG_FILE_NAME AS (
     LEFT JOIN t_doc_fil df ON df.c_id_1 = d.c_id
     LEFT JOIN t_file_dat f ON df.c_id_2 = f.c_id
 )
-SELECT e.eco_number,
-       e.eco_doc_intgr_job_status,
-       e.eco_doc_intgr_job_err_mess,
-       d.c_id AS doc_cid,
-       d.document_id,
-       d.doc_version,
-       d.pmt_stbn_eng,
-       d.free_name,
+SELECT d.c_id AS doc_cid,
        d.doc_type,
        d.doc_subtype,
        d.cax_type,
+       d.sheet_no,
        f.file_format,
        f.c_id AS file_cid,
-       d.edb_id,
-       ofn.org_file_name       
+       df.pos_no,
+       ofn.org_file_name,
+       f.file_type       
 FROM t_doc_dat d
-LEFT JOIN t_erp_eco_doc e ON d.c_id = e.doc_cid
 LEFT JOIN ORG_FILE_NAME ofn ON d.c_id = ofn.doc_cid
 LEFT JOIN t_doc_fil df ON d.c_id = df.c_id_1
 LEFT JOIN t_file_dat f ON df.c_id_2 = f.c_id;
