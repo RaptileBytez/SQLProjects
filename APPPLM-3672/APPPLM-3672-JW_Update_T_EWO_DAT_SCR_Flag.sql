63e97c6d5071e4a277f153b295064c42ea1a8396
RaptileBytez <jesco.wurm@marel.com>
2025-10-06 13:56:55 +0200

    [main] APPPLM-3672 Release

--- APPPLM-3672/APPPLM-3672-JW_Update_T_EWO_DAT_SCR_Flag.sql
+++ APPPLM-3672/APPPLM-3672-JW_Update_T_EWO_DAT_SCR_Flag.sql
@@ -1,35 +1,13 @@
 /*
 ===============================================================================
-Script Name: APPPLM-3672-JW_PT2_Update_T_EWO_DAT_SCR_Flag.sql
-===============================================================================
-Description:   Update T_EWO_DAT.SCR flag based on T_EWR_DAT.SCR flag, except for 
-               ELEM_IDs that exist multiple times in T_EWO_DAT. These will be 
-               reported with the APPPLM-3672-JW_PT1_Dublicate_ELEM_ID_Report.sql script.
-Author:        Jesco Wurm (ICP)
-Creation Date: 24-09-2025
-Version:        1.0.0
-Last Modified: 24-09-2025
-Last Modified By: Jesco Wurm (ICP)
---------------------------------------------------------------------------------
-Purpose: 
-This script will 
-- update the SCR flag in T_EWO_DAT based on the SCR flag in T_EWR_DAT for all ELEM_IDs 
-    that exist only once in T_EWO_DAT.
-- print out all updated ELEM_IDs and their new SCR value.
---------------------------------------------------------------------------------  
-Change History:
-===============================================================================
- */
-/*
-===============================================================================
-Script Name: APPPLM-3672-JW_PT2_Update_T_EWO_DAT_SCR_Flag.sql
+Script Name: APPPLM-3672-JW_Update_T_EWO_DAT_SCR_Flag.sql
 ===============================================================================
 Description:    Update T_EWO_DAT.SCR flag based on T_EWR_DAT.SCR flag, except for
                 ELEM_IDs that exist multiple times in T_EWO_DAT. These will be
                 reported with the APPPLM-3672-JW_PT1_Dublicate_ELEM_ID_Report.sql script.
 Author:         Jesco Wurm (ICP)
-Creation Date: 24-09-2025
-Version:         1.0.1
+Creation Date: 06-10-2025
+Version:         1.0.2
 Last Modified: 24-09-2025
 Last Modified By: Jesco Wurm (ICP)
 --------------------------------------------------------------------------------
@@ -43,6 +21,7 @@ Change History:
 Version    Date        Author         Description
 V1.0.0    24-09-2025  Jesco Wurm    Initial creation
 V1.0.1    24-09-2025  Jesco Wurm    Redesigne from MERGE to CURSER due to limitation of RETURNING clause in Oracle
+V1.0.2    06-10-2025  Jesco Wurm    Renamed Script & changed output to English
 ===============================================================================
   */
 SET SERVEROUTPUT ON;
@@ -85,7 +64,7 @@ BEGIN
     -- log the updated C_ID and new SCR value
     DBMS_OUTPUT.PUT_LINE(
       'Updated C_ID: ' || rec.C_ID ||
-      ', Neuer SCR-Wert: ' || rec.EWR_SCR
+      ', New SCR value: ' || rec.EWR_SCR
     );
   END LOOP;
   
@@ -94,6 +73,6 @@ BEGIN
   
   -- print summary
   DBMS_OUTPUT.PUT_LINE(CHR(10) || '------------------------------');
-  DBMS_OUTPUT.PUT_LINE('Gesamte Anzahl der aktualisierten Zeilen: ' || updated_count);
+  DBMS_OUTPUT.PUT_LINE('Total number of updated rows: ' || updated_count);
 END;
 /
